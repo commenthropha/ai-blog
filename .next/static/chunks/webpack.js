d49f300ad179abb0fc68568fe20d724032537de3
Anees <aharoon1007@gmail.com>
2023-01-02 20:30:07 +0000

    Update layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c1725479348bd86"; }
+/******/ 		__webpack_require__.h = function() { return "773771d007454663"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
